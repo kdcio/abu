e1ff47a21dfe85dd9a731e56b2c177024099bd96
Ian Dela Cruz <iandc76@gmail.com>
2021-03-28 06:30:27 +0800

    initial cms base

---
+++ packages/cms/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Abu</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
