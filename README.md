27e3a9d6bf4a57361536b425668640568507a1aa
Ian Dela Cruz <iandc76@gmail.com>
2021-03-28 09:05:28 +0800

    Full cognito setup

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# abu
-Abu CMS is a Serverless Headless CMS
+# Abu CMS
+
+Serverless Headless CMS
+
+## Development
+
+1. Create `config/dev.yml` and add `REGION` and `PROFILE`.
+2. Setup cognito
+
+   ```bash
+   yarn setup dev your@email.com
+   ```
--- scripts/setup-cog.sh
+++ scripts/setup-cog.sh
@@ -1,7 +1,36 @@
 #!/bin/bash
 
+# Colors
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+Blue='\033[0;34m'
+NC='\033[0m' # No Color
+
+if [ -z "$1" ]
+  then
+    echo -e "\nThis script will provision Cognitor resources in AWS and create the first user.\n"
+    echo -e "Usage: ./setup-cog.sh ${Blue}<stage> <email>${NC}\n"
+    echo -e "Example: ./setup-cog.sh staging jon@doe.com\n"
+  exit 1
+fi
+
+if [ -z "$2" ]
+  then
+    echo -e "\nMissing ${RED}email${NC}\n"
+  exit 1
+fi
+
 STAGE=$1
+EMAIL=$2
 POOL_KEY="AbuCognitoUserPoolId-$STAGE"
+
+regex="^(([A-Za-z0-9]+((\.|\-|\_|\+)?[A-Za-z0-9]?)*[A-Za-z0-9]+)|[A-Za-z0-9]+)@(([A-Za-z0-9]+)+((\.|\-|\_)?([A-Za-z0-9]+)+)*)+\.([A-Za-z]{2,})+$"
+
+if [[ ! $EMAIL =~ ${regex} ]]; then
+  echo -e "\nInvalid email: ${RED}${EMAIL}${NC}\n"
+  exit 1
+fi
+
 
 REGION_MATCH="REGION: "
 REGION=$(grep "$REGION_MATCH" config/$STAGE.yml | sed -e "s/^$REGION_MATCH//")
@@ -9,7 +38,8 @@ REGION=$(grep "$REGION_MATCH" config/$STAGE.yml | sed -e "s/^$REGION_MATCH//")
 PROFILE_MATCH="PROFILE: "
 PROFILE=$(grep "$PROFILE_MATCH" config/$STAGE.yml | sed -e "s/^$PROFILE_MATCH//")
 
-# yarn workspace abu-res setup:cog dev
+echo -e "\n${Blue}Setting up cognito...${NC}\n"
+yarn workspace abu-res setup:cog dev
 
 # Get export list
 EXP_LIST=$(aws cloudformation list-exports --output text --profile $PROFILE --region $REGION)
@@ -17,4 +47,12 @@ EXP_LIST=$(aws cloudformation list-exports --output text --profile $PROFILE --re
 # Get Pool Id
 POOL_ID=$(echo "$EXP_LIST" | grep $POOL_KEY | awk 'END {print $4}')
 
-echo $POOL_ID
+echo -e "\n${Blue}Create cognito user...${NC}\n"
+aws cognito-idp admin-create-user \
+    --profile $PROFILE \
+    --region $REGION \
+    --user-pool-id $POOL_ID \
+    --username $2
+
+echo -e "\n\n${GREEN}Setup Cognito Success!!!${NC}\n"
+echo -e "A temporary password has been sent to your email. You can now login to your AbuCMS.\n"
