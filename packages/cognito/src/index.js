a071507c59fbb316a9b77aae60e426e29d761da8
Ian Dela Cruz <iandc76@gmail.com>
2021-04-01 06:40:47 +0800

    Add cognito groups

--- packages/cognito/src/index.js
+++ packages/cognito/src/index.js
@@ -1,6 +1,8 @@
 import create from './create';
 import list from './list';
 import remove from './remove';
+import read from './read';
+import groups from './groups';
 import setGroup from './setGroup';
 import updateAttributes from './updateAttributes';
 import changePassword from './changePassword';
@@ -9,6 +11,8 @@ export default {
   create,
   list,
   remove,
+  read,
+  groups,
   setGroup,
   updateAttributes,
   changePassword,
