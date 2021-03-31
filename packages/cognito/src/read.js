9feb01c7abedc541e5ae6fb80c0325ef305e5d48
Ian Dela Cruz <iandc76@gmail.com>
2021-04-01 06:35:41 +0800

    Format user data

--- packages/cognito/src/read.js
+++ packages/cognito/src/read.js
@@ -6,12 +6,31 @@ const read = async ({ email }) => {
   if (!process.env.COG_POOL_ID) throw new Error('Missing Cognito Pool Id');
   if (!email) throw new Error('Missing email');
 
-  await cognito
+  const data = await cognito
     .adminGetUser({
       UserPoolId: process.env.COG_POOL_ID,
       Username: email,
     })
     .promise();
+
+  const groups = await cognito
+    .adminListGroupsForUser({
+      UserPoolId: process.env.COG_POOL_ID,
+      Username: email,
+    })
+    .promise();
+
+  return {
+    created: data.UserCreateDate,
+    updated: data.UserLastModifiedDate,
+    statues: data.UserStatus,
+    username: data.Username,
+    ...data.UserAttributes.reduce((acc, attr) => {
+      acc[attr.Name] = attr.Value;
+      return acc;
+    }, {}),
+    groups: groups?.Groups.map((g) => g.GroupName),
+  };
 };
 
 export default read;
--- packages/cognito/tests/read.test.js
+++ packages/cognito/tests/read.test.js
@@ -12,21 +12,57 @@ describe('Read user', () => {
 
   it('should read user', async () => {
     process.env.COG_POOL_ID = 'pool-id';
+    const email = 'john@doe.com';
+    const user = {
+      Username: 'fc2c8bb1-ada6-41d7-8451-fee42ad0d5d7',
+      UserAttributes: [
+        { Name: 'sub', Value: 'fc2c8bb1-ada6-41d7-8451-fee42ad0d5d7' },
+        { Name: 'given_name', Value: 'John' },
+        { Name: 'family_name', Value: 'Doe' },
+        { Name: 'email', Value: email },
+      ],
+      UserCreateDate: '2021-03-29T01:05:14.954Z',
+      UserLastModifiedDate: '2021-03-29T01:12:50.287Z',
+      Enabled: true,
+      UserStatus: 'CONFIRMED',
+    };
+    const groups = {
+      Groups: [
+        {
+          GroupName: 'admin',
+          UserPoolId: 'ap-southeast-1_xxxxxxxxx',
+          LastModifiedDate: '2021-03-29T00:53:47.634Z',
+          CreationDate: '2021-03-29T00:53:47.634Z',
+        },
+      ],
+    };
     const fakeCog = {
       adminGetUser: jest.fn().mockReturnValue({
-        promise: () => Promise.resolve(),
+        promise: () => Promise.resolve(user),
+      }),
+      adminListGroupsForUser: jest.fn().mockReturnValue({
+        promise: () => Promise.resolve(groups),
       }),
     };
     init.mockImplementation(() => fakeCog);
 
-    const email = 'john@doe.com';
-    await read({ email });
+    const res = await read({ email });
     expect(init).toBeCalledTimes(1);
     expect(fakeCog.adminGetUser).toBeCalledTimes(1);
     expect(fakeCog.adminGetUser).toBeCalledWith({
       UserPoolId: 'pool-id',
       Username: email,
     });
+
+    expect(res.created).toBe(user.UserCreateDate);
+    expect(res.updated).toBe(user.UserLastModifiedDate);
+    expect(res.statues).toBe(user.UserStatus);
+    expect(res.username).toBe(user.Username);
+    expect(res.sub).toBe('fc2c8bb1-ada6-41d7-8451-fee42ad0d5d7');
+    expect(res.given_name).toBe('John');
+    expect(res.family_name).toBe('Doe');
+    expect(res.email).toBe(email);
+    expect(res.groups).toEqual(['admin']);
   });
 
   it('should throw missing pool id', async () => {
