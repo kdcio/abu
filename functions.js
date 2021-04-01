80b62729f9de95cb8809312ae4e58666db56b597
Ian Dela Cruz <iandc76@gmail.com>
2021-04-01 09:07:19 +0800

    Add local api

---
+++ functions.js
@@ -0,0 +1,40 @@
+const fs = require("fs");
+const yaml = require("yaml");
+
+const renameFunctions = (serviceName, basePath, funcs) => {
+  const keys = Object.keys(funcs);
+  const functions = {};
+  keys.forEach((k) => {
+    const func = funcs[k];
+    func.handler = `${basePath}${func.handler}`;
+    if (func.events) {
+      func.events = func.events.map((e) => {
+        if (!e.http) return e;
+        e.http.path = `${serviceName}${e.http.path}`;
+        return e;
+      });
+    }
+    functions[`${serviceName}-${k}`] = funcs[k];
+  });
+  return functions;
+};
+
+module.exports = () => {
+  const services = ["users"];
+
+  let functions = {};
+  services.forEach((serviceName) => {
+    const basePath = `./packages/api-${serviceName}/`;
+    let file = fs.readFileSync(`${basePath}functions.yml`, "utf8");
+    file = file.replace(/\$\{self\:service\}/g, serviceName);
+    file = file.replace(/\$\{self\:provider.stage\}/g, "dev");
+    const funcs = yaml.parse(file);
+
+    functions = {
+      ...functions,
+      ...renameFunctions(serviceName, basePath, funcs),
+    };
+  });
+
+  return functions;
+};
