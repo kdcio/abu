4b64a7abc5482696a593c193ff1482ebaeea1606
Ian Dela Cruz <iandc76@gmail.com>
2021-04-02 12:50:05 +0800

    Change modal title on select field

--- packages/cms/src/pages/system/Models/AddField.js
+++ packages/cms/src/pages/system/Models/AddField.js
@@ -2,11 +2,33 @@ import React from "react";
 
 import { CModal, CModalBody, CModalHeader, CModalTitle } from "@coreui/react";
 import { useModal } from "context/modal";
-import { SelectProvider } from "context/select";
+import { useSelect } from "context/select";
 import FieldSelector from "components/system/FieldSelector";
 
 const AddField = () => {
   const { modal, setModal } = useModal();
+  const { selected } = useSelect();
+
+  let title = "Add field";
+  switch (selected) {
+    case "date":
+      title = "Add date field";
+      break;
+    case "image":
+      title = "Add image field";
+      break;
+    case "rich-text":
+      title = "Add rich text field";
+      break;
+    case "slug":
+      title = "Add slug field";
+      break;
+    case "text":
+      title = "Add text field";
+      break;
+    default:
+      break;
+  }
 
   return (
     <CModal
@@ -15,13 +37,9 @@ const AddField = () => {
       closeOnBackdrop={false}
     >
       <CModalHeader closeButton>
-        <CModalTitle>Add field</CModalTitle>
+        <CModalTitle>{title}</CModalTitle>
       </CModalHeader>
-      <CModalBody>
-        <SelectProvider>
-          {modal === "addModelField" && <FieldSelector />}
-        </SelectProvider>
-      </CModalBody>
+      <CModalBody>{modal === "addModelField" && <FieldSelector />}</CModalBody>
     </CModal>
   );
 };
--- packages/cms/src/pages/system/Models/index.js
+++ packages/cms/src/pages/system/Models/index.js
@@ -3,6 +3,7 @@ import React from "react";
 import { CRow, CCol } from "@coreui/react";
 import { ListProvider } from "context/list";
 import { ModalProvider } from "context/modal";
+import { SelectProvider } from "context/select";
 import List from "./List";
 import Form from "./Fields";
 import Add from "./Add";
@@ -14,16 +15,18 @@ const Models = () => {
   return (
     <ListProvider>
       <ModalProvider>
-        <CRow>
-          <CCol sm="4">
-            <List />
-          </CCol>
-          <CCol sm="8">
-            <Form />
-          </CCol>
-        </CRow>
-        <Add />
-        <AddField />
+        <SelectProvider>
+          <CRow>
+            <CCol sm="4">
+              <List />
+            </CCol>
+            <CCol sm="8">
+              <Form />
+            </CCol>
+          </CRow>
+          <Add />
+          <AddField />
+        </SelectProvider>
       </ModalProvider>
     </ListProvider>
   );
