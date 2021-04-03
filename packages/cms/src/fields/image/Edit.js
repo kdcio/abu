002e5a11a37a776ab3542fe5f77e032f965c987c
Ian Dela Cruz <iandc76@gmail.com>
2021-04-03 16:27:52 +0800

    Image edit field

--- packages/cms/src/fields/image/Edit.js
+++ packages/cms/src/fields/image/Edit.js
@@ -1,7 +1,64 @@
-import React from "react";
+import React, { useState, useCallback, useEffect } from "react";
+import { CFormGroup, CLabel } from "@coreui/react";
+import { useFormContext } from "react-hook-form";
+import { useDropzone } from "react-dropzone";
+import { useFileReader } from "utils/useFileReader";
 
-const Edit = () => {
-  return <h3>Image edit</h3>;
+import "scss/components/image-edit.scss";
+
+const Edit = ({ name, id, validations, help }) => {
+  const [image, setImage] = useState(null);
+  const { register, errors, setValue } = useFormContext();
+  const [{ error, loading }, setFile] = useFileReader({
+    method: "readAsDataURL",
+    onload: setImage,
+  });
+
+  const onDrop = useCallback(
+    (acceptedFiles) => {
+      setFile(acceptedFiles[0]);
+      setValue(id, acceptedFiles[0]);
+    },
+    [id, setFile, setValue]
+  );
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+
+  useEffect(() => {
+    register({ name: id }, { required: validations.required });
+  }, [id, register, validations.required]);
+
+  return (
+    <CFormGroup>
+      <CLabel htmlFor="name">
+        {name} {validations?.required && "*"}
+      </CLabel>
+      <div className="image-update" {...getRootProps()}>
+        {loading ? <p>Reading file</p> : null}
+        {error ? <p>{error.message}</p> : null}
+        {image ? (
+          <div className="d-flex flex-column align-items-center">
+            <img className="preview-img" src={image} alt="Preview Logo" />
+            <p>Click here to change</p>
+          </div>
+        ) : (
+          <div className="card">
+            <div className="card-body">
+              {isDragActive ? (
+                <p>Drop the image here ...</p>
+              ) : (
+                <p>Drag 'n' drop your image here, or click to select</p>
+              )}
+            </div>
+          </div>
+        )}
+        <input {...getInputProps()} />
+      </div>
+      {errors[id] && (
+        <div className="invalid-feedback">This field is required.</div>
+      )}
+      {help && <small className="form-text text-muted">{help}</small>}
+    </CFormGroup>
+  );
 };
 
 export default Edit;
