3b7196c834bbae7513ca7ff3b7b27aff0760d285
Ian Dela Cruz <iandc76@gmail.com>
2021-04-03 15:30:29 +0800

    Add rich text field

--- packages/cms/src/fields/rich-text/Edit.js
+++ packages/cms/src/fields/rich-text/Edit.js
@@ -1,7 +1,38 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { CFormGroup, CLabel } from "@coreui/react";
+import { useFormContext } from "react-hook-form";
+import ReactQuill from "react-quill";
 
-const Edit = () => {
-  return <h3>rich text edit</h3>;
+import "react-quill/dist/quill.snow.css";
+import "scss/components/quill.scss";
+
+const Edit = ({ name, id, validations, help }) => {
+  const { register, setValue, errors } = useFormContext();
+
+  useEffect(() => {
+    register({ name: id }, { required: validations.required });
+  }, [id, register, validations.required]);
+
+  return (
+    <CFormGroup>
+      <CLabel htmlFor="name">
+        {name} {validations?.required && "*"}
+      </CLabel>
+      <ReactQuill
+        theme="snow"
+        id={id}
+        name={id}
+        defaultValue="Please write something"
+        onChange={(content) => {
+          setValue(id, content);
+        }}
+      />
+      {errors[id] && (
+        <div className="invalid-feedback">This field is required.</div>
+      )}
+      {help && <small className="form-text text-muted">{help}</small>}
+    </CFormGroup>
+  );
 };
 
 export default Edit;
