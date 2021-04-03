903cd64d47ecf5d6ae1131331d868d6ea182fcb2
Ian Dela Cruz <iandc76@gmail.com>
2021-04-03 14:48:48 +0800

    Edit date field

--- packages/cms/src/fields/date/Edit.js
+++ packages/cms/src/fields/date/Edit.js
@@ -1,7 +1,37 @@
 import React from "react";
+import { CFormGroup, CLabel } from "@coreui/react";
+import { useFormContext } from "react-hook-form";
+import { dateFormatInput } from "utils/dateFormat";
 
-const Edit = () => {
-  return <h3>Date edit</h3>;
+const Edit = ({ name, id, validations, help, today }) => {
+  const { register, errors, formState } = useFormContext();
+  const { isSubmitting } = formState;
+
+  let defaultValue = null;
+  if (today) {
+    defaultValue = dateFormatInput(new Date());
+  }
+
+  return (
+    <CFormGroup>
+      <CLabel htmlFor="name">
+        {name} {validations?.required && "*"}
+      </CLabel>
+      <input
+        type="date"
+        className={`form-control ${errors[id] && "is-invalid"}`}
+        id={id}
+        name={id}
+        ref={register({ required: validations.required })}
+        defaultValue={defaultValue}
+        disabled={isSubmitting}
+      />
+      {errors[id] && (
+        <div className="invalid-feedback">This field is required.</div>
+      )}
+      {help && <small className="form-text text-muted">{help}</small>}
+    </CFormGroup>
+  );
 };
 
 export default Edit;
