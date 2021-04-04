599aa5e07eb3c36ee9d3aa62471de2579ca86819
Ian Dela Cruz <iandc76@gmail.com>
2021-04-04 18:59:58 +0800

    Update working

--- packages/cms/src/pages/system/ApiAccess/Form.js
+++ packages/cms/src/pages/system/ApiAccess/Form.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import {
   CButton,
   CCard,
@@ -21,77 +21,63 @@ import { useModels } from "context/models";
 import { useToaster } from "context/toaster";
 
 import create from "api/create";
-// import get from "api/get";
-// import update from "api/update";
+import get from "api/get";
+import update from "api/update";
 
 const Form = () => {
   const { id } = useParams();
   const { list: models } = useModels();
   const history = useHistory();
   const { addToast } = useToaster();
-  const { register, handleSubmit, errors } = useForm();
+  const { register, handleSubmit, errors, getValues, reset } = useForm();
   const [processing, setProcessing] = useState(false);
   const [error, setError] = useState(null);
+  const [key, setKey] = useState(null);
 
   const switchRef = useRef([]);
 
-  // const updateUser = async ({ firstName, lastName, group }) => {
-  //   const data = {};
-  //   if (
-  //     firstName !== defaultValues.firstName ||
-  //     lastName !== defaultValues.lastName
-  //   ) {
-  //     data.firstName = firstName;
-  //     data.lastName = lastName;
-  //   }
+  const createAccess = async (data) => {
+    const { id } = await create({ apiName: "Access", data });
+    addToast({
+      message: "API Access saved successfully!",
+      color: "success",
+    });
+    history.push(`/system/api_access/edit/${id}`);
+  };
 
-  //   if (group !== defaultValues.group) {
-  //     data.newGroup = group;
-  //     data.oldGroup = defaultValues.group;
-  //   }
-
-  //   if (Object.keys(data).length <= 0) return;
-  //   // send only changed data
-  //   await update({
-  //     apiName: "Users",
-  //     id,
-  //     data,
-  //   });
-  // };
+  const updateAccess = async (data) => {
+    await update({ apiName: "Access", id, data });
+    addToast({
+      message: "API Access saved successfully!",
+      color: "success",
+    });
+    setProcessing(false);
+  };
 
   const onSubmit = async (data) => {
     setProcessing(true);
     try {
-      await create({ apiName: "Access", data });
-      addToast({
-        message: "API Access saved successfully!",
-        color: "success",
-      });
-      // TODO: redirect to edit to show key
-      history.push("/system/api_access");
+      if (id) updateAccess(data);
+      else createAccess(data);
     } catch (err) {
       setError(err);
       setProcessing(false);
     }
   };
 
-  // useEffect(() => {
-  //   const getUser = async () => {
-  //     const res = await get({ apiName: "Users", id });
-  //     const values = {
-  //       ...getValues(),
-  //       firstName: res.given_name,
-  //       lastName: res.family_name,
-  //       group: res.groups?.[0] || "editor",
-  //       email: res.email,
-  //     };
-  //     setDefaultValues(values);
-  //     reset(values);
-  //     setProcessing(false);
-  //   };
-  //   if (id) getUser();
-  //   else history.push("/system/users");
-  // }, [id, getValues, reset, history]);
+  useEffect(() => {
+    const getAccess = async () => {
+      const res = await get({ apiName: "Access", id });
+      const values = {
+        ...getValues(),
+        ...res,
+      };
+      reset(values);
+      setKey(res.key);
+      setProcessing(false);
+    };
+    if (id) getAccess();
+  }, [id, getValues, reset, history]);
 
   return (
     <form onSubmit={handleSubmit(onSubmit)} autoComplete="off">
@@ -127,7 +113,13 @@ const Form = () => {
                 <CFormGroup>
                   <CLabel htmlFor="api-key">API Key</CLabel>
                   <CInputGroup>
-                    <CInput type="text" id="api-key" name="api-key" readOnly />
+                    <CInput
+                      type="text"
+                      id="api-key"
+                      name="api-key"
+                      readOnly
+                      value={key}
+                    />
                     <CInputGroupAppend>
                       <CButton type="button" color="info">
                         Copy
@@ -195,7 +187,7 @@ const Form = () => {
           </CButton>
           <Link
             id="cancel"
-            to={`/system/users`}
+            to={`/system/api_access`}
             className="btn btn-danger btn-sm"
             disabled={processing}
           >
