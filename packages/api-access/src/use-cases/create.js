71eb10b55468cdecdefa3831643374b3e816ea70
Ian Dela Cruz <iandc76@gmail.com>
2021-04-06 04:13:38 +0800

    Remove dash in key Fixes #47

--- packages/api-access/src/use-cases/create.js
+++ packages/api-access/src/use-cases/create.js
@@ -1,7 +1,7 @@
 const makeCreate = ({ model, uuid }) => {
   const create = async ({ ...data }) => {
     const id = new Date().valueOf();
-    const key = uuid();
+    const key = uuid().replace(/-/g, "");
     await model.put({ ...data, id, key });
     return { id };
   };
