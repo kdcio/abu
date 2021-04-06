c57742a9623b7933543832986775b2c4d40f68a1
Ian Dela Cruz <iandc76@gmail.com>
2021-04-07 04:46:38 +0800

    Add authorizer to scripts

--- scripts/test-api.sh
+++ scripts/test-api.sh
@@ -2,6 +2,7 @@
 
 yarn workspace cognito test
 yarn workspace model test
+yarn workspace authorizer test
 yarn workspace api-user test
 yarn workspace api-model test
 yarn workspace api-content test
