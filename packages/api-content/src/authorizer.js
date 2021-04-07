438875756c74f07946c89eea910ccfb381cc6865
Ian Dela Cruz <iandc76@gmail.com>
2021-04-08 06:00:18 +0800

    Fix authorizer debug

--- packages/api-content/src/authorizer.js
+++ packages/api-content/src/authorizer.js
@@ -10,7 +10,7 @@ export const handler = async (event) => {
     const { authorizationToken: token, methodArn } = event;
     if (!token) throw new Error("Unauthorized");
     const policy = await authorizer({ token, methodArn });
-    debug(JSON.stringify(policy, null, 2));
+    debug(JSON.stringify(policy));
     return policy;
   } catch (error) {
     debug(error);
