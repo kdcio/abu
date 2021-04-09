63c61d6f907184bc0e95a08f801d7151e47720d6
Ian Dela Cruz <iandc76@gmail.com>
2021-04-10 06:25:46 +0800

    Remove error log in test

--- packages/authorizer/tests/setup.js
+++ packages/authorizer/tests/setup.js
@@ -1,5 +1,5 @@
 process.env.DDB_ENDPOINT = 'http://localhost:8062';
 process.env.DDB_REGION = 'localhost';
 process.env.DDB_TABLE = 'abu-test-authorizer';
-process.env.DEBUG = 'lambda:error';
+process.env.DEBUG = '';
 process.env.PAGINATION_SECRET = 'fge34534tgdfg';
