8bd55427f0364bdc826fa0956750b4408b8d483e
Ian Dela Cruz <iandc76@gmail.com>
2021-04-19 10:15:51 +0800

    Use one array in seeding

--- packages/api-admin/src/use-cases/seed.js
+++ packages/api-admin/src/use-cases/seed.js
@@ -4,26 +4,23 @@ import contents from "../seed/content.json";
 
 const makeSeed = ({ Model, ApiAccess, Content, uuid }) => {
   const seed = async () => {
-    const modelProms = [];
-    models.forEach((model) => modelProms.push(Model.put(model)));
-    await Promise.all(modelProms);
+    const proms = [];
+    models.forEach((model) => proms.push(Model.put(model)));
 
-    const accessProms = [];
     accesses.forEach((access) => {
       const id = new Date().valueOf().toString();
       const key = uuid().replace(/-/g, "");
-      accessProms.push(ApiAccess.put({ ...access, id, key }));
+      proms.push(ApiAccess.put({ ...access, id, key }));
     });
-    await Promise.all(accessProms);
 
-    const contentProms = [];
     contents.forEach((content) => {
       const { modelId, id: inId, ...data } = content;
       const id = inId || new Date().valueOf().toString();
-      contentProms.push(Content.put({ ...data, id, modelId }));
+      proms.push(Content.put({ ...data, id, modelId }));
     });
-    await Promise.all(contentProms);
+    await Promise.all(proms);
   };
   return seed;
 };
+
 export default makeSeed;
