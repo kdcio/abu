18102764040f1214b2b4e13841074bb6325fdfda
Ian Dela Cruz <iandc76@gmail.com>
2021-05-31 06:38:59 +0800

    Create cognito

---
+++ infra/lib/api.ts
@@ -0,0 +1,98 @@
+import * as cdk from "@aws-cdk/core";
+import * as cognito from "@aws-cdk/aws-cognito";
+
+export class APIStack extends cdk.NestedStack {
+  userPool: cognito.UserPool;
+  userPoolClient: cognito.CfnUserPoolClient;
+
+  constructor(scope: cdk.Construct, id: string, props?: cdk.NestedStackProps) {
+    super(scope, id, props);
+
+    // high level construct
+    this.userPool = new cognito.UserPool(this, id + "Pool", {
+      signInAliases: {
+        email: true,
+      },
+      autoVerify: {
+        email: true,
+      },
+    });
+
+    // any properties that are not part of the high level construct can be added using this method
+    const userPoolCfn = this.userPool.node.defaultChild as cognito.CfnUserPool;
+    userPoolCfn.userPoolAddOns = { advancedSecurityMode: "ENFORCED" };
+    userPoolCfn.schema = [
+      {
+        name: "given_name",
+        attributeDataType: "String",
+        mutable: true,
+        required: true,
+      },
+      {
+        name: "family_name",
+        attributeDataType: "String",
+        mutable: true,
+        required: true,
+      },
+    ];
+    userPoolCfn.accountRecoverySetting = {
+      recoveryMechanisms: [
+        {
+          name: "verified_email",
+          priority: 1,
+        },
+      ],
+    };
+    userPoolCfn.usernameAttributes = ["email"];
+    userPoolCfn.policies = {
+      passwordPolicy: {
+        minimumLength: 6,
+        requireLowercase: false,
+        requireNumbers: true,
+        requireSymbols: false,
+        requireUppercase: true,
+      },
+    };
+
+    // create two groups, one for admins one for users
+    // these groups can be used without configuring a 3rd party IdP
+
+    // new cognito.CfnUserPoolGroup(this, "AdminsGroup", {
+    //   groupName: adminsGroupName,
+    //   userPoolId: userPool.userPoolId,
+    // });
+
+    // new cognito.CfnUserPoolGroup(this, "UsersGroup", {
+    //   groupName: usersGroupName,
+    //   userPoolId: userPool.userPoolId,
+    // });
+
+    const supportedIdentityProviders = ["COGNITO"];
+
+    let appUrl = "https://abu-cms.com";
+
+    this.userPoolClient = new cognito.CfnUserPoolClient(
+      this,
+      "CognitoAppClientCDK",
+      {
+        supportedIdentityProviders: supportedIdentityProviders,
+        clientName: "Web",
+        allowedOAuthFlowsUserPoolClient: true,
+        allowedOAuthFlows: ["implicit"],
+        allowedOAuthScopes: ["phone", "email", "openid", "profile"],
+        explicitAuthFlows: ["ALLOW_REFRESH_TOKEN_AUTH"],
+        preventUserExistenceErrors: "ENABLED",
+        generateSecret: false,
+        refreshTokenValidity: 1,
+        callbackUrLs: [appUrl],
+        logoutUrLs: [appUrl],
+        userPoolId: this.userPool.userPoolId,
+      }
+    );
+
+    // we want to make sure we do things in the right order
+    // if (cognitoIdp) {
+    //   cfnUserPoolClient.node.addDependency(cognitoIdp);
+    // }
+  }
+}
--- infra/lib/infra-stack.ts
+++ infra/lib/infra-stack.ts
@@ -1,12 +1,15 @@
 import * as cdk from "@aws-cdk/core";
 import { CMSStack } from "./cms";
+import { APIStack } from "./api";
 
 export class InfraStack extends cdk.Stack {
   cms: CMSStack;
+  api: APIStack;
 
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     this.cms = new CMSStack(this, "CMS");
+    this.api = new APIStack(this, "API");
   }
 }
