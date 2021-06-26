ea7b5641286296c6568a6535663e5f0187fe7f95
Ian Dela Cruz <iandc76@gmail.com>
2021-06-26 15:02:35 +0800

    Add env variables

--- scripts/cdk-to-config.js
+++ scripts/cdk-to-config.js
@@ -1,12 +1,13 @@
 const path = require("path");
 const fs = require("fs");
 const crypto = require("crypto");
+const dotenv = require("dotenv");
 const yaml = require("yaml");
 const cdkOutput = require("../infra/.infra-output.json");
 
-const fileName = path.join(__dirname, `../config/${process.env.ABU_STAGE}.yml`);
-const file = fs.readFileSync(fileName, "utf8");
-const configs = yaml.parse(file);
+let fileName = path.join(__dirname, `../config/${process.env.ABU_STAGE}.yml`);
+let file = fs.readFileSync(fileName, "utf8");
+let configs = yaml.parse(file);
 
 const stack = cdkOutput["AbuStack-github"];
 configs.COG_POOL_ID = stack.COGPOOLID;
@@ -14,10 +15,34 @@ configs.COG_POOL_CLIENT_ID = stack.COGPOOLCLIENTID;
 configs.COG_POOL_CLIENT_DOMAIN = stack.COGPOOLCLIENTDOMAIN;
 configs.COG_ACCESS_KEY_ID = stack.COGACCESSKEYID;
 configs.COG_SECRET_ACCESS_KEY = stack.COGSECRETACCESSKEY;
-configs.COG_OAUTH_CALLBACK = `https://${stack.COGPOOLCLIENTID}`;
+configs.COG_OAUTH_CALLBACK = `https://${stack.CFURL}`;
 configs.CF_ID = stack.CFID;
 configs.PAGINATION_SECRET = crypto.randomBytes(32).toString("hex");
 configs.DDB_TABLE = process.env.PROJECT;
 
-const newConfigs = yaml.stringify(configs);
+let newConfigs = yaml.stringify(configs);
+fs.writeFileSync(fileName, newConfigs, { encoding: "utf8" });
+
+fileName = path.join(
+  __dirname,
+  `../packages/cms/.env.production.${process.env.ABU_STAGE}`
+);
+file = fs.readFileSync(fileName, "utf8");
+configs = dotenv.parse(file);
+
+configs.REACT_APP_AUTH_AWS_REGION = process.env.AWS_REGION;
+configs.REACT_APP_AUTH_POOL_ID = stack.COGPOOLID;
+configs.REACT_APP_AUTH_CLIENT_ID = stack.COGPOOLCLIENTID;
+configs.REACT_APP_AUTH_OAUTH_DOMAIN = `${stack.COGPOOLCLIENTDOMAIN}.auth.${process.env.AWS_REGION}.amazoncognito.com`;
+configs.REACT_APP_AUTH_OAUTH_SIGNIN = `https://${stack.CFURL}`;
+configs.REACT_APP_AUTH_OAUTH_SIGNOUT = `https://${stack.CFURL}`;
+configs.REACT_APP_UPLOAD_ENDPOINT = configs.REACT_APP_UPLOAD_ENDPOINT || "";
+configs.REACT_APP_API_ENDPOINT = configs.REACT_APP_API_ENDPOINT || "";
+
+newConfigs = ``;
+
+Object.keys(configs).forEach(
+  (key) => (newConfigs += `${key}=${configs[key]}\n`)
+);
+
 fs.writeFileSync(fileName, newConfigs, { encoding: "utf8" });
